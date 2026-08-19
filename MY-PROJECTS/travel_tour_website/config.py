0ef6b23c422c86c50659e22bdbd77a19985239b6
nirali vala <niralivala2006@gmail.com>
2026-08-19 20:23:58 +0530

    Delete MY-PROJECTS/travel_tour_website directory

--- MY-PROJECTS/travel_tour_website/config.py
+++
@@ -1,8 +0,0 @@
-DB_CONFIG = {
-    'user': 'root',
-    'password': '',
-    'host': '127.0.0.1',
-    'database': 'travel_system',
-    'raise_on_warnings': True
-}
-SECRET_KEY = 'change-this-to-a-secure-key'
--- MY-PROJECTS/travel_tour_website/tesy_db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("travel.db")
-cursor = conn.cursor()
-
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-print("📋 Tables:", cursor.fetchall())
-
-conn.close()
